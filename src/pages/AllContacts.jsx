c246670351a0df85f59ec73194ee3b15b4078405
Md. Shoaib Akhter Shifat <sifuisu3@gmail.com>
2024-01-07 11:04:20 +0600

    on large screen grid columns set to 3

--- src/pages/AllContacts.jsx
+++ src/pages/AllContacts.jsx
@@ -21,15 +21,19 @@ const AllContacts = () => {
   }, [setContacts]);
 
   return (
-    <section className="h-full flex flex-col items-center justify-center gap-4">
+    <section className="container mx-auto p-4 h-full flex flex-col items-center justify-center gap-4">
       <h1 className="text-3xl font-bold underline underline-offset-4 text-[#4F6F52] mt-4">
         All Contacts
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {contacts.map((contact) => (
-          <Card key={contact?._id} contact={contact} />
-        ))}
-      </div>
+      {!contacts ? (
+        <span>Loading...</span>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {contacts.map((contact) => (
+            <Card key={contact?._id} contact={contact} />
+          ))}
+        </div>
+      )}
     </section>
   );
 };
